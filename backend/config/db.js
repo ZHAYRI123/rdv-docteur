b4cf31f3353af78d5dbd34913cb05027bf1d8782
ch13ckak <kratosto446@gmail.com>
2025-03-22 00:55:30 +0000

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
 //Crée un fichier config/db.js pour connecter ton serveur Express à MongoDB avec Mongoose.
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
