c66fda78e6139db17fe3493648079c9061985f12
unknown <merykozan@gmail.com>
2025-03-22 13:33:43 +0000

    creer server.js

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,1 +1,18 @@
-//Maintenant, dans ton fichier server.js, tu dois importer et utiliser ces configurations.
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
+//import royes creer (ex:import routes from './routes/index.js';)
+
+dotenv.config();
+const app = express();
+
+// Connexion MongoDB
+connectDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(Serveur sur http://localhost:${PORT}));
