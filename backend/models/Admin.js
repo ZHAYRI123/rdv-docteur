ac07f311a281d28d3dc569be393854d4764352ee
Sparte <kratosto446@gmail.com>
2025-03-26 22:12:04 +0000

    modification de int en string

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 export const AdminSchema = new Schema({
-  id: { type: Number, required: true },
+  id: { type: String, required: true },
   nom: { type: String, required: true },
   prenom: { type: String, required: true },
   email: { type: String, required: true },
