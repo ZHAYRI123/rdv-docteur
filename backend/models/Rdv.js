3a671734bb91bf96df7462bc075a38c5a548233d
Sparte <kratosto446@gmail.com>
2025-03-27 15:47:04 +0000

    modif

--- backend/models/Rdv.js
+++ backend/models/Rdv.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
-import { PatientSchema } from './Patient.js';
+import Patient from './Patient.js';
+import Docteur from './Docteur.js';
 
 export const RdvSchema = new Schema({
   id: { type: String, required: true },
   date: { type: Date, required: true },
   heure: { type: String, required: true },
-  patient_id: { type: PatientSchema, required: true },
+  patient_id: { type: Patient, required: true },
+  docteur_id: { type: Docteur, required: true }
 });
 
 modules.exports = mongoose.model('Rdv', RdvSchema);
