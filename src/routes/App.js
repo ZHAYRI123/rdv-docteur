8332198d2ea61ce1c211d1f1ff47999b7d5c96bc
unknown <merykozan@gmail.com>
2025-04-06 18:42:56 +0000

    routes login

---
+++ src/routes/App.js
@@ -0,0 +1,18 @@
+import Login from './login.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/doctor-login' element={<Login isDoctor={true} />} />
+					<Route path='/patient-login' element={<Login isDoctor={false} />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
