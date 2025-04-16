620f43d9c1a8442c95f6fb03d4dd88850b82d2cc
Sparte <kratosto446@gmail.com>
2025-04-16 18:10:28 +0100

    react

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,4 +1,9 @@
 import Login from './login.jsx';
+import Acc from './Acc.jsx';
+import Cards from './card.jsx';
+import Features from './Features.jsx';
+import Footer from './footer.jsx';
+import Header from './Header.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 	return (
@@ -11,6 +16,20 @@ function App() {
 					<Route path='/patient-login' element={<Login isDoctor={false} />} />
 				</Routes>
 			</BrowserRouter>
+			<div className="bg-zinc-50">
+      <Header />
+      <main className="pt-20">
+        <Acc />
+        <Cards />
+        <Features />
+      </main>
+      <Footer />
+    </div>
+			<Router>
+				<Routes>
+					<Route path='/Hopital' element={<HopitalPage />} />
+				</Routes>
+			</Router>
 		</>
 
 
