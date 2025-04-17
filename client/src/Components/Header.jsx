e01c8d22b27345c91f514103d4fdde1c833db6e0
Sparte <kratosto446@gmail.com>
2025-04-17 22:07:42 +0100

    tailwind

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,10 +1,11 @@
+import logo from "../image/logo.png";
 function Header() {
   return (
     <header className="fixed top-0 w-full bg-white shadow-lg z-50">
       <nav className="container mx-auto px-4 py-3">
         <div className="flex justify-between items-center">
           <a href="/" className="flex items-center gap-2">
-            <img src="./logo.png" alt="Basmah Logo" className="h-16 w-32" />
+            <img src={logo} alt="Basmah Logo" className="h-16 w-32" />
           </a>
           <div className="space-x-6">
             <a href="#" className="text-zinc-900 hover:text-blue-500 transition-colors">Home</a>
